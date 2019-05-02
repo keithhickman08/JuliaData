6ad30f6f862280c3d39addf2b1a089b87471bed6
keithhickman08 <keonhick@iu.edu>
2019-05-01 22:10:50 -0400

    separate traintest function

--- lib/MyLib.jl
+++ lib/MyLib.jl
@@ -5,7 +5,6 @@ using MLDataUtils
 using DelimitedFiles
 
 function GetData()
-  mkdir("data")
   a = readdlm(download("https://archive.ics.uci.edu/ml/machine-learning-databases/00484/tripadvisor_review.csv"), ',', header=false)
   open("data/travel.txt", "w") do f
     writedlm(f, a, ",")
